d627bd3172ee63f20bfd5d38a33869203b3d115a
허연우 <jftj97@gmail.com>
2022-02-15 17:47:14 +0900

    set basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { LandModule } from './land/land.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
+import { LandEntity } from './land/entities/land.entity';
 
 @Module({
-  imports: [UserModule, LandModule],
+  imports: [
+    UserModule,
+    LandModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      name: 'mysql',
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.MySQL_USERNAME,
+      password: process.env.MySQL_PW,
+      database: 'newsland',
+      entities: [UserEntity, LandEntity],
+      synchronize: true,
+      charset: 'utf8mb4',
+      autoLoadEntities: true,
+      logging: false,
+      keepConnectionAlive: true,
+      timezone: 'Asia/Seoul',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
+
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('NewsLand API')
+    .setDescription('nestjs API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // const firebaseConfig = require('./service-account-credentials.json');
+  // logger.info(firebaseConfig);
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(firebaseConfig),
+  // });
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
+
+  //typeORM 도입시 keepConnectionAlive: true 추가
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 bootstrap();
